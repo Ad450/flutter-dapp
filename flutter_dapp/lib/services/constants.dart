b39abe1591c53ec6dfd3c9a4ac7a00e846205e58
Ad450 <adjeimanue@gmail.com>
2022-04-10 23:33:44 +0000

    app constants

---
+++ flutter_dapp/lib/services/constants.dart
@@ -0,0 +1,3 @@
+const kblockchainUrl = "https://rinkeby.infura.io/v3/57e4dfc483fd42ca88ad0f55abc9b524";
+const kPrivateKey = "d4503b8ecba0da2ea82d9868e150a3cfda7e075ed68a40ffa2336f7c976762ce";
+const kContractAddress = "0x059755341bd7973baa98508315C930996a0BC851";
